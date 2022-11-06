4c15f166d7a7f3f6b79b8cdadd9e9fc95d144884
Olamide O <59629698+olamide87@users.noreply.github.com>
2022-11-06 02:56:33 +0000

    added file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import Favorites from './components/Favorites'
+import Meals from './components/Meals'
+import Modal from './components/Modal'
+import Search from './components/Search'
 
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      {/*<Search/>*/}
+      {/*<Favorites/>*/}
+      <Meals/>
+    {/*<Modal/>*/}
     </main>
   )
 }
