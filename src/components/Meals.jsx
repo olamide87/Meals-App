0e79d0281b98df0d8b537a3008516e738e69d526
Olamide O <59629698+olamide87@users.noreply.github.com>
2022-11-08 05:09:35 +0000

    use Context

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,8 +1,7 @@
-import {useContext} from 'react'
-import {AppContext} from '../context'
+import {useGlobalContext} from '../context'
 
 const Meals = () => {
-  const context = useContext(AppContext)
+  const context = useGlobalContext()
   console.log(context)
   return <h1>Meals Component </h1> 
 }
--- src/context.jsx
+++ src/context.jsx
@@ -1,11 +1,20 @@
-import React, {useContext} from 'react'
+import React, {useContext,useEffect} from 'react'
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  useEffect(()=>{
+    console.log('fetch data here')
+  },[]) 
   return <AppContext.Provider value={{name:'john', role:'student'}}>
     {children}
   </AppContext.Provider>
 }
+
+export const useGlobalContext = () =>{
+  return useContext(AppContext)
+}
+
+
 
 export { AppContext, AppProvider }
