2466b33ddb4723366e87125d7dd0ea3936df6e54
Olamide O <59629698+olamide87@users.noreply.github.com>
2022-11-12 06:21:05 +0000

    Added styling for cards and button,

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -3,10 +3,17 @@ import {useGlobalContext} from '../context'
 const Meals = () => {
   const {meals} = useGlobalContext()
 
-  return <section>
+  return <section className='section-center'>
     {meals.map((singleMeal)=>{
-    console.log(singleMeal)
-    return <h4>single meal</h4>
+    const {idMeal, strMeal:title, strMealThumb:image} = singleMeal
+    
+    return <article key={idMeal} className="single-meal">
+    <img src={image} className="img" style={{width:"200px"}}/>
+      <footer>
+      <h5>{title}</h5>
+        <button className="like-btn">click me</button>
+        </footer>
+      </article>
     })}
     </section>
 }
