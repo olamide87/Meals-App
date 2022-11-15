a68d1c41d48955ae0d90def4b5b9a3093a4c9763
Olamide O <59629698+olamide87@users.noreply.github.com>
2022-11-15 06:41:40 +0000

    added like button

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import {useGlobalContext} from '../context'
+import {BsHandThumbsUp} from 'react-icons/bs'
 
 const Meals = () => {
   const {meals} = useGlobalContext()
@@ -11,7 +12,7 @@ const Meals = () => {
     <img src={image} className="img" style={{width:"200px"}}/>
       <footer>
       <h5>{title}</h5>
-        <button className="like-btn">click me</button>
+        <button className="like-btn"><BsHandThumbsUp/></button>
         </footer>
       </article>
     })}
