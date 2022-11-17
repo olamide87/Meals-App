7f1b8c9231b2bc91ee632c45830cb7df8175d900
Olamide O <59629698+olamide87@users.noreply.github.com>
2022-11-17 00:39:12 +0000

    added search text box and button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,14 @@
+import {useState} from 'react'
+import {useGlobalContext} from '../context'
+
 const Search = () => {
-  return <h1>Search Component</h1>
+  return <header className="search-container">
+    <form>
+      <input type="text" placeholder='type favorite meal' className='form-input'/>
+      <button type="submit" className='btn'>search</button>
+      <button type="submit" className='btn btn-hipster'>surprise me</button>
+    </form>
+    </header>
 }
 
 export default Search
